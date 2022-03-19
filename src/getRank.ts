39ff3773ccd7c06ce5d0b47ce697bcab47fb645e
HCLonely <h1606051253@gmail.com>
2022-03-19 17:53:33 +0800

    修复BUG

--- src/getRank.ts
+++ src/getRank.ts
@@ -307,8 +307,9 @@ axios.get('https://gamewith.jp/pricone-re/article/show/93068')
         .includes('docs/cdn/')) {
         const version = new Date().getTime();
         fs.writeFileSync('./docs/cdn/version', `${version}`);
-        fs.writeFileSync('./docs/raw.html', fs.readFileSync('./docs/raw.html').toString()
-          .replaceAll('@main', `@${version}`));
       }
+      fs.writeFileSync('./docs/raw.html', fs.readFileSync('./docs/raw.html').toString()
+        .replaceAll('@main', `@${fs.readFileSync('./docs/cdn/version').toString()
+          .trim()}`));
     }
   });
