c584a31a9fff827a7ef7e8843a433017c9cc841e
HCLonely <h1606051253@gmail.com>
2022-03-19 17:38:30 +0800

    部署测试

--- src/pushDocs.js
+++ src/pushDocs.js
@@ -3,26 +3,34 @@ const { spawnSync, execSync } = require('child_process');
 const fs = require('fs');
 
 const { GITHUB_TOKEN } = process.env;
+const gitSpawn = (param1, param2, param3) => {
+  const [, stdout, stderr] = spawnSync(param1, param2, param3);
+  return `stdout:
+${stdout.toString()}
+
+stderr:
+${stderr.toString()}`;
+};
 
 const gitStatus = execSync('git status -s').toString();
 
 if (!gitStatus) return;
-console.log(spawnSync('git', ['add', '.']).output);
-console.log(spawnSync('git', ['commit', '-m', 'Daily Sync']).output);
-console.log(spawnSync('git', ['push', 'origin', 'main']).output);
+console.log(gitSpawn('git', ['add', '.']));
+console.log(gitSpawn('git', ['commit', '-m', 'Daily Sync']));
+console.log(gitSpawn('git', ['push', 'origin', 'main']));
 
 if (gitStatus.includes('docs/cdn/')) {
   const version = fs.readFileSync('docs/cdn/version').toString();
-  console.log(spawnSync('git', ['add', '.']).output);
-  console.log(spawnSync('git', ['tag', '-a', version, '-m', 'update']).output);
-  console.log(spawnSync('git', ['push', 'origin', version]).output);
+  console.log(gitSpawn('git', ['add', '.']));
+  console.log(gitSpawn('git', ['tag', '-a', version, '-m', 'update']));
+  console.log(gitSpawn('git', ['push', 'origin', version]));
 }
 
 if (gitStatus.includes('docs/')) {
-  console.log(spawnSync('git', ['init'], { cwd: 'docs' }).output);
-  console.log(spawnSync('git', ['add', '-A'], { cwd: 'docs' }).output);
-  console.log(spawnSync('git', ['commit', '-m', 'Daily Sync'], { cwd: 'docs' }).output);
-  console.log(spawnSync('git', ['push', '-u', `"https://x-access-token:${GITHUB_TOKEN}@github.com/HCLonely/pcr-jp-rank.git"`, 'HEAD:gh-pages', '--force'], { cwd: 'docs' }).output); // eslint-disable-line
+  console.log(gitSpawn('git', ['init'], { cwd: 'docs' }));
+  console.log(gitSpawn('git', ['add', '-A'], { cwd: 'docs' }));
+  console.log(gitSpawn('git', ['commit', '-m', 'Daily Sync'], { cwd: 'docs' }));
+  console.log(gitSpawn('git', ['push', '-u', `"https://x-access-token:${GITHUB_TOKEN}@github.com/HCLonely/pcr-jp-rank.git"`, 'HEAD:gh-pages', '--force'], { cwd: 'docs' })); // eslint-disable-line
 }
 
 /*
